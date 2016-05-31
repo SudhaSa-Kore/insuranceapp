486983cd533db6a09cd9c66bb533c762e06fda2d
Kstream 001 <kstream001@gmail.com>
2016-05-31 13:01:38 +0530

    Newactionadded

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/users', users);
 
 app.get('/getPremiumDate',function(req,res){
 	res.writeHead(200,{'Content-Type':'application/json'});
-	res.end(JSON.stringify({"policies":[{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/5/2019","premiumPaymentDate":"27/5/2017"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"},{"policyNumber":"1987650","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"2/6/2016"}]}));
+	res.end(JSON.stringify({"policies":[{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/5/2019","premiumPaymentDate":"27/5/2017"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"},{"policyNumber":"19876589","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"2/6/2016"}]}));
 });
 
 
@@ -41,6 +41,19 @@ app.get('/payPremiumamount',function(req,res){
 app.get('/getTaxCertificate',function(req,res){
 	res.writeHead(200,{'Content-Type':'application/json'});
 	res.end(JSON.stringify({"policyNumber":"9348209","policyName":"Jeevan Anandh","url":"http://www.revenue.ie/images/guide-paye-employees-booklet.jpg"}));
+
+});
+
+app.get('/getPolicyDetails/:policyId',function(req,res){
+	
+	res.writeHead(200,{'Content-Type':'application/json'});
+	if(req.params.policyId == "9348209")
+	res.end(JSON.stringify({"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/5/2019","premiumPaymentDate":"27/5/2017"}));
+    else if(req.params.policyId == "1987650")
+	res.end(JSON.stringify({"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"}));
+    else if(req.params.policyId == "19876589")
+	res.end(JSON.stringify({"policyNumber":"19876589","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"2/6/2016"}));
+    
 
 });
 // catch 404 and forward to error handler
