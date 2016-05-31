3d1145c4c221221203e3a4f33f93d2d24c27acca
Kstream 001 <kstream001@gmail.com>
2016-05-31 18:01:40 +0530

    'Newfieldsaddedd

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/users', users);
 
 app.get('/getPremiumDate',function(req,res){
 	res.writeHead(200,{'Content-Type':'application/json'});
-	res.end(JSON.stringify({"policies":[{"policyNumber":"19876589","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"12/6/2016"},{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/7/2019","premiumPaymentDate":"27/5/2017"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"}]}));
+	res.end(JSON.stringify({"policies":[{"policyNumber":"19876589","policyStartDate":"2/7/2007","policyType":"Quarterly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","policyMaturityDate":"2/7/2024","premiumPaymentDate":"12/6/2016"},{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"Yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/7/2019","premiumPaymentDate":"27/5/2017","policyMaturityDate":"27/7/2021"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"Half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016","policyMaturityDate":"27/5/2030"}]}));
 });
 
 
